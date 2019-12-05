1328d9f8215835fb1951a306fb4c40c076c5f965
Shane Auckland <shane.auckland@anaplan.com>
2019-12-05 16:44:58 +0000

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -10,11 +10,28 @@ For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel re
 For a mass of 1969, the fuel required is 654.
 For a mass of 100756, the fuel required is 33583.
 
-The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually 
-calculate the fuel needed for the mass of each module (your puzzle input), then add together 
-all the fuel values.
+Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and 
+subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any 
+mass that would require negative fuel should instead be treated as if it requires zero fuel; the 
+remaining mass, if any, is instead handled by wishing really hard, which has no mass and is 
+outside the scope of this calculation.
 
-What is the sum of the fuel requirements for all of the modules on your spacecraft?
+So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount 
+you just calculated as the input mass and repeat the process, continuing until a fuel requirement 
+is zero or negative. For example:
+
+A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded 
+down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel 
+(654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which 
+requires no further fuel. So, the total fuel required for a module of mass 1969 is 
+654 + 216 + 70 + 21 + 5 = 966.
+The fuel required by a module of mass 100756 and its fuel is: 
+33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+What is the sum of the fuel requirements for all of the modules on your spacecraft when also 
+taking into account the mass of the added fuel? (Calculate the fuel requirements for each module 
+separately, then add them all up at the end.)
 */
 
 use std::fs::File;
@@ -30,13 +47,22 @@ pub fn run() {
 
     for line in reader.lines() {
         let line = line.expect("Unable to read line");
-        total += calculate_fuel_requirement(line.parse::<i32>().unwrap());
+        total += calculate_total_fuel_requirement(line.parse::<i32>().unwrap());
     }   
 
     println!("The total fuel requirements are {}", total);
 }
 
-fn calculate_fuel_requirement(mass: i32) -> i32 {
+fn calculate_total_fuel_requirement(mut mass: i32) -> i32 {
+    let mut total: i32 = 0;
+    while mass >= 8 {
+        mass = calculate_single_fuel_requirement(mass);
+        total += mass;
+    }
+    return total;
+}
+
+fn calculate_single_fuel_requirement(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
@@ -46,11 +72,21 @@ mod tests {
 
     #[test]
     fn it_handles_a_mass_that_divides_by_3() {
-        assert_eq!(calculate_fuel_requirement(12), 2);
+        assert_eq!(calculate_single_fuel_requirement(12), 2);
     }
 
     #[test]
     fn it_handles_a_mass_that_does_not_divide_by_3() {
-        assert_eq!(calculate_fuel_requirement(16), 3);
+        assert_eq!(calculate_single_fuel_requirement(16), 3);
+    }
+
+    #[test]
+    fn it_does_not_add_fuel_for_a_tiny_mass() {
+        assert_eq!(calculate_total_fuel_requirement(2), 0);
+    }
+
+    #[test]
+    fn it_adds_fuel_weight() {
+        assert_eq!(calculate_total_fuel_requirement(1969), 966);
     }
 }
